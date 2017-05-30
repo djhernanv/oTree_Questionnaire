a64947edf0fdeaa8b9339dd7f7aaffe43ce07897
Hernan Villamizar <hernan.villamizar@gmail.com>
2017-05-30 17:03:33 +0200

    Added App Files

---
+++ models.py
@@ -0,0 +1,63 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Hernan Villamizar'
+
+doc = """
+Questionnaire for Skill and Fairness
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'questionnaire'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    luck = models.PositiveIntegerField(
+        choices=[
+            [1, 'Absolutely Not'],
+            [2, ''],
+            [3, ''],
+            [4, ''],
+            [5, ''],
+            [6, ''],
+            [7, ''],
+            [8, ''],
+            [9, ''],
+            [10, 'Exclusively'],
+        ],
+        # horizontal radio button instead of selection
+        widget=widgets.RadioSelectHorizontal()
+    )
+    skill = models.PositiveIntegerField(
+        choices=[
+            [1, 'Absolutely Not'],
+            [2, ''],
+            [3, ''],
+            [4, ''],
+            [5, ''],
+            [6, ''],
+            [7, ''],
+            [8, ''],
+            [9, ''],
+            [10, 'Exclusively'],
+        ],
+        # horizontal radio button instead of selection
+        widget=widgets.RadioSelectHorizontal()
+    )
+    ave = models.PositiveIntegerField()
+    bestB = models.PositiveIntegerField()
+    bestA = models.PositiveIntegerField()
---
+++ views.py
@@ -0,0 +1,17 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class questionnaire(Page):
+    pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [
+    questionnaire
+]
